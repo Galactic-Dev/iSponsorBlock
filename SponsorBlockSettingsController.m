87fda8cb9fa9fda794331e6737d84a2fe87ab762
dayanch96 <38832025+dayanch96@users.noreply.github.com>
2023-06-01 16:37:00 +0300

    Fix sections count

--- SponsorBlockSettingsController.m
+++ SponsorBlockSettingsController.m
@@ -121,12 +121,12 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 17;
+    return 18;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (section == 0) return 1;
-    else if (section <= 6 || section == 16) return 2;
+    else if (section <= 6 || section == 17) return 2;
     return 1;
 }
 
@@ -246,7 +246,7 @@
         [textCell editableTextField].delegate = self;
         return textCell;
     }
-    if (indexPath.section >= 11 && indexPath.section < 16) {
+    if (indexPath.section >= 11 && indexPath.section < 17) {
         NSArray *titles = @[LOC(@"ShowSkipNotice"), LOC(@"ShowButtonsInPlayer"), LOC(@"HideStartEndButtonInPlayer"), LOC(@"ShowModifiedTime"), LOC(@"AudioNotificationOnSkip"), LOC(@"EnableSkipCountTracking")];
         NSArray *titlesNames = @[@"showSkipNotice", @"showButtonsInPlayer", @"hideStartEndButtonInPlayer", @"showModifiedTime", @"skipAudioNotification", @"enableSkipCountTracking"];
         UITableViewCell *tableCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SponsorBlockCell3"];
@@ -265,7 +265,7 @@
         }
         return tableCell;
     }
-    if (indexPath.section == 16) {
+    if (indexPath.section == 17) {
         UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"SponsorBlockDonationCell"];
         cell.textLabel.text = indexPath.row == 0 ? LOC(@"DonateOnVenmo") : LOC(@"DonateOnPayPal");
         cell.imageView.image = [UIImage systemImageNamed:@"dollarsign.circle.fill"];
@@ -290,7 +290,7 @@
 
 //To allow highlights only for certain sections
 - (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 16) {
+    if (indexPath.section == 17) {
         return YES;
     } else {
         return NO;
@@ -302,7 +302,7 @@
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.section == 16) {
+    if (indexPath.section == 17) {
         if (indexPath.row == 0) {
             if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"venmo://"]]) {
                 [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"venmo://venmo.com/code?user_id=3178620965093376215"] options:@{} completionHandler:nil];
